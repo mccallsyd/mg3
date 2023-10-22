f90aa4f70ceb44ecd02e0dc0efd21d7e478fdda3
Sydney McCall <mccallsyd@gmail.com>
2023-10-22 01:33:38 -0400

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,10 +195,8 @@ var mobileMenu = document.querySelector(".mobile-menu");
 mobileMenuBtn.addEventListener("click", () => {
   if (mobileMenu.style.display === "none") {
     mobileMenu.style.display = "flex";
-    mobileMenuBtn.innerHTML = "Close";
   } 
   else {
     mobileMenu.style.display = "none";
-    mobileMenuBtn.innerHTML = "Menu";
   }
 });
